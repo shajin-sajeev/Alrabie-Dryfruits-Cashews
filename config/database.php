008829e94c7967b0ce965f1a096d28fd223a4ef9
shajin-sajeev <mail2shajinsajeev@gmail.com>
2026-02-12 15:23:28 +0530

    ltst

--- config/database.php
+++ config/database.php
@@ -28,7 +28,7 @@ return [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => env('DB_DATABASE', 'forge') . (str_contains(env('DB_HOST', ''), 'neon.tech') ? "' options='endpoint=" . explode('.', env('DB_HOST', ''))[0] : ""),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
