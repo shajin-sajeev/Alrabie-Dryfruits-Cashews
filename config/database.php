e1ca1276c4e1f0ab06af728c1d80152a6868d224
shajin-sajeev <mail2shajinsajeev@gmail.com>
2026-02-12 15:03:03 +0530

    ltst

--- config/database.php
+++ config/database.php
@@ -26,19 +26,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('DATABASE_URL'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'require',
-            'options' => [
-                '--endpoint=ep-plain-bird-ai5s9cny-pooler',
-            ],
         ],
     ],
 ];
