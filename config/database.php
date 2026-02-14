d5305495aa455186c7d2f5ea82948bf1f465abfc
shajin-sajeev <mail2shajinsajeev@gmail.com>
2026-02-14 16:43:49 +0530

    end point issue

--- config/database.php
+++ config/database.php
@@ -37,11 +37,9 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'require',
-            'options' => array_filter([
-                'endpoint' => (str_contains(env('DB_HOST', ''), 'neon.tech')
-                    ? explode('.', str_replace('-pooler', '', env('DB_HOST', '')))[0]
-                    : null),
-            ]),
+            'options' => (str_contains(env('DB_HOST', ''), 'neon.tech')
+                ? "endpoint=" . explode('.', str_replace('-pooler', '', env('DB_HOST', '')))[0]
+                : null),
         ],
     ],
 ];
